dadba63b4290127f88e67ca0d9328a0b3b8287e8
David Kröll <david.kroell@outlook.com>
2020-03-31 15:22:16 +0200

    Version v1.0.0 Add descriptions Add interactive password input if missing Remove cobra config file
    Signed-off-by: David Kröll <david.kroell@outlook.com>

--- cmd/upload.go
+++ cmd/upload.go
@@ -2,22 +2,24 @@ package cmd
 
 import (
 	"github.com/davidkroell/bodycomposition"
+	"golang.org/x/crypto/ssh/terminal"
+	"syscall"
 
 	"github.com/spf13/cobra"
 )
 
 // uploadCmd represents the upload command
 var uploadCmd = &cobra.Command{
-	Use:   "upload",
-	Short: "Upload your body values to Garmin connect cloud",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:     "upload",
+	Short:   "Upload your body composition values to Garmin Connect",
+	Aliases: []string{"u", "add"},
 	Run: func(cmd *cobra.Command, args []string) {
 		weight, _ := cmd.Flags().GetFloat64("weight")
+		if weight == -1 {
+			cmd.PrintErr("No weight specified\n")
+			return
+		}
+
 		fat, _ := cmd.Flags().GetFloat64("fat")
 		hydration, _ := cmd.Flags().GetFloat64("hydration")
 		bone, _ := cmd.Flags().GetFloat64("bone")
@@ -27,6 +29,14 @@ to quickly create a Cobra application.`,
 
 		email, _ := cmd.Flags().GetString("email")
 		password, _ := cmd.Flags().GetString("password")
+		if password == "" {
+			cmd.Print("Password for ", email, ": ")
+			bytePasswd, err := terminal.ReadPassword(int(syscall.Stdin))
+			if err != nil {
+				cmd.PrintErr("Password input failed\n")
+			}
+			password = string(bytePasswd)
+		}
 
 		bc.UploadWeight(email, password)
 	},
@@ -38,7 +48,7 @@ func init() {
 	uploadCmd.Flags().StringP("email", "e", "", "Email of the Garmin account")
 	uploadCmd.Flags().StringP("password", "p", "", "Password of the Garmin account")
 
-	uploadCmd.Flags().Float64P("weight", "w", 0, "Set your weight in kilograms")
+	uploadCmd.Flags().Float64P("weight", "w", -1, "Set your weight in kilograms")
 	uploadCmd.Flags().Float64P("fat", "f", 0, "Set your fat in percent")
 	uploadCmd.Flags().Float64("hydration", 0, "Set your hydration in percent")
 	uploadCmd.Flags().Float64P("bone", "b", 0, "Set your bone mass in percent")
